0fe5df3f0919b5cf3e4a4e3779b1e361f552ee81
Zane <zanewnch@gmail.com>
2024-03-22 21:12:33 +0800

    add tailwind to project.

---
+++ my-app/app/official/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet, Button, View, TextInput } from "react-native";
+
+export default function official() {
+  return (
+    <View>
+      <View>
+        <TextInput />
+        <Button title="Add" />
+      </View>
+
+      <View></View>
+    </View>
+  );
+}
--- my-app/app/prac/index.tsx
+++ my-app/app/prac/index.tsx
@@ -1,33 +1,44 @@
 /* 
 1. state practice with useEffect
 2. css 套用
+3. using tailwind css with twrnc library, the syntax is 
+<Text style={tw`bg-red-300 justify-center item-center text-blue-300`}>
+
+key point
+文字一定要丟在 Text 裡面
+不能放在View 
+
+
 */
-import { View,Text,Button,StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
-export default function prac(){
-    // state
-    const [outputText,setOutputText] = useState("The initial text.")
+import tw from "twrnc";
+export default function prac() {
+  // state
+  const [outputText, setOutputText] = useState("The initial text.");
 
-    // real-time render with useEffect
-    useEffect(()=>{},[outputText]);
+  // real-time render with useEffect
+  useEffect(() => {}, [outputText]);
 
-    const handleClick = ()=>{
-        setOutputText("change value.")
-    }
-    
+  const handleClick = () => {
+    setOutputText("change value.");
+  };
 
-    // using css
-    const styles = StyleSheet.create({
-        container:{
-            color:"red",
-            backgroundColor:"gray"
-        }
-    })
-    
-    return (
-        <View>
-            <Text style={styles.container}>{outputText}</Text>
-            <Button title="click me" onPress={handleClick}/>
-        </View>
-    );
+  // using css
+  const styles = StyleSheet.create({
+    container: {
+      color: "red",
+      backgroundColor: "gray",
+    },
+  });
+
+  return (
+    <View>
+      <Text style={styles.container}>{outputText}</Text>
+      <Button title="click me" onPress={handleClick} />
+      <Text style={tw`bg-red-300 justify-center item-center text-blue-300`}>
+        tailwind test
+      </Text>
+    </View>
+  );
 }
